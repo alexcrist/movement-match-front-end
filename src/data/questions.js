cf7f45c9c4eca22ba26b1871bff5f50f556ca0d2
Alex Crist <Alex_Crist@intuit.com>
2018-06-29 14:16:48 -0700

    Add mock question group data

---
+++ src/data/questions.js
@@ -0,0 +1,56 @@
+export default [
+  {
+    groupName: 'Basic questions',
+    questions: [
+      {
+        title: 'What is your name?',
+        answers: [
+          {
+            title: 'alex'
+          },
+          {
+            title: 'jim'
+          }
+        ]
+      },
+      {
+        title: 'What is your favorite color?',
+        answers: [
+          {
+            title: 'blue'
+          },
+          {
+            title: 'red'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    groupName: 'Food questions',
+    questions: [
+      {
+        title: 'What is your favorite pasta?',
+        answers: [
+          {
+            title: 'rigotoni'
+          },
+          {
+            title: 'spaghetti'
+          }
+        ]
+      },
+      {
+        title: 'What is your favorite ice cream flavor?',
+        answers: [
+          {
+            title: 'vanilla'
+          },
+          {
+            title: 'mint'
+          }
+        ]
+      }
+    ]
+  }
+];
